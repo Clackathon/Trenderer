cabc48ea22faa3b810f6bdf58330b4d647a9e2b7
Euan <masonator117@gmail.com>
2016-02-20 17:28:48 +0000

    Calc average 2.0

--- src/main/java/com/clackathon/vuzii/Relation.java
+++ src/main/java/com/clackathon/vuzii/Relation.java
@@ -1,6 +1,7 @@
 package com.clackathon.vuzii;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -8,12 +9,15 @@ import java.util.List;
  */
 public class Relation {
 
-	public double relativity(){
-		double relative = 0;
-
-		Image imageA;
-		Image imageB;
+	public double relativity(Image a, Image b){
 		List<Double> percentages = new ArrayList<>();
+
+		percentages.add(getTagRelation(a.getTags(), b.getTags()));
+		percentages.add(getUploaderRelation(a.getUploader(), b.getUploader()));
+		percentages.add(getCreationTimeRelation(a.getCreationTime(), b.getCreationTime()));
+		percentages.add(getLocationRelation(a.getLocation(), b.getLocation()));
+		percentages.add(getCommentsRelation(a.getComments(), b.getComments()));
+
 		/*
 			tags 0.4
 			uploader 0.8
@@ -26,6 +30,33 @@ public class Relation {
 		return calculateAverage(percentages);
 	}
 
+	private double getCommentsRelation(List<String> commentsA, List<String> commentsB) {
+		List<String> intersection = new ArrayList<>(commentsA);
+		intersection.retainAll(commentsB);
+		return ((double) intersection.size())/Math.max(commentsA.size(),commentsB.size());
+	}
+
+	private double getLocationRelation(Location locationA, Location locationB) {
+		
+		return
+	}
+
+	private Double getCreationTimeRelation(Date creationTime, Date creationTime1) {
+
+		return
+	}
+
+	private double getUploaderRelation(User uploaderA, User uploaderB) {
+
+		return
+	}
+
+	private double getTagRelation(List<String> a, List<String> b) {
+		List<String> intersection = new ArrayList<>(a);
+		intersection.retainAll(b);
+		return ((double) intersection.size())/Math.max(a.size(),b.size());
+	}
+
 	private static double calculateAverage(List<Double> values) {
 		double sum = 0;
 		if(!values.isEmpty()) {
@@ -36,6 +67,7 @@ public class Relation {
 		}
 		return sum;
 	}
+
 
 
 }
