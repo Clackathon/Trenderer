8933ca9fd7be2b4deb92cd7c601240a2d7e1de43
Ross Allan <rallanpcl@gmail.com>
2016-02-20 23:11:33 +0000

    Add (broken) Instagram Image Provider

---
+++ src/main/java/com/clackathon/vuzii/api/ImageProvider.java
@@ -0,0 +1,9 @@
+package com.clackathon.vuzii.api;
+
+import com.clackathon.vuzii.Image;
+
+import java.util.List;
+
+public interface ImageProvider {
+	List<Image> getImages();
+}
---
+++ src/main/java/com/clackathon/vuzii/api/InstagramImageProvider.java
@@ -0,0 +1,49 @@
+package com.clackathon.vuzii.api;
+
+import com.clackathon.vuzii.Image;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.jinstagram.Instagram;
+import org.jinstagram.auth.InstagramAuthService;
+import org.jinstagram.auth.model.Verifier;
+
+import java.io.FileInputStream;
+import java.util.List;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class InstagramImageProvider implements ImageProvider {
+	private Instagram instagram = getInstagramAuthService();
+	final Properties apiProperties = loadProperties();
+
+	@SneakyThrows
+	private Properties loadProperties() {
+		val p = new Properties();
+		p.load(new FileInputStream("./secrets/"));
+		return p;
+	}
+
+	private Instagram getInstagramAuthService() {
+		val service =  new InstagramAuthService()
+			.apiKey(apiProperties.getProperty("INSTAGRAM_KEY"))
+			.apiSecret(apiProperties.getProperty("INSTAGRAM_SECRET"))
+			.callback("http://localhost/")
+			.build();
+
+		String authorizationUrl = service.getAuthorizationUrl();
+
+		System.out.println("Visit " + authorizationUrl);
+		System.out.println("Enter verifier code: ");
+
+		String verifierCode = new Scanner(System.in).next();
+
+		val verifier = new Verifier(verifierCode);
+		return new Instagram(service.getAccessToken(verifier));
+	}
+
+	@SneakyThrows
+	public List<Image> getImages() {
+		System.out.println(instagram.getUserRecentMedia());
+		throw new UnsupportedOperationException("todo");
+	}
+}
