536542b178bb6bcdb632acd67cabaa1ebb4f0024
Ross Allan <rallanpcl@gmail.com>
2016-02-21 00:24:40 +0000

    Split up testGetImages due to reason relating to NoSuchElementException

--- src/test/java/com/clackathon/vuzii/api/instagram/InstagramImageProviderTest.java
+++ src/test/java/com/clackathon/vuzii/api/instagram/InstagramImageProviderTest.java
@@ -1,11 +1,14 @@
 package com.clackathon.vuzii.api.instagram;
 
+import lombok.val;
 import org.junit.Test;
 
 public class InstagramImageProviderTest {
 
 	@Test
 	public void testGetImages() throws Exception {
-		System.out.println(new InstagramImageProvider().getImages());
+		val provider = new InstagramImageProvider();
+		val images = provider.getImages();
+		System.out.println(images);
 	}
 }
