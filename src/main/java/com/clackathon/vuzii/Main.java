e0858d07c4098753d6b7893a0745619db1e03606
Ross Allan <rallanpcl@gmail.com>
2016-02-21 05:56:43 +0000

    JSONify

--- src/main/java/com/clackathon/vuzii/Main.java
+++ src/main/java/com/clackathon/vuzii/Main.java
@@ -2,16 +2,22 @@ package com.clackathon.vuzii;
 
 import com.clackathon.vuzii.api.ImageProvider;
 import com.clackathon.vuzii.api.flatfile.FlatFileImageProvider;
+import lombok.SneakyThrows;
 import lombok.val;
+import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.File;
 import java.util.List;
 
 public class Main {
+	@SneakyThrows
 	public static void main(String[] args) {
 		ImageProvider provider = new FlatFileImageProvider();
 		List<Image> images = provider.getImages();
 		val linkifier = new Linkifier(images, new Relation());
 		linkifier.linkify();
 		linkifier.dumpStats();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.writerWithDefaultPrettyPrinter().writeValue(new File("./build/linked.json"), linkifier.images);
 	}
 }
